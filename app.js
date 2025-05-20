1406380eb6f31c8daa92c6a907aa9a0d5f477775
Warren Wong <117635685+huangweicom@users.noreply.github.com>
2025-05-20 21:11:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 // 中间件配置
 app.use(cors());
